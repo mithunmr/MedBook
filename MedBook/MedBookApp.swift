24f1cd6cd175d4eb0b08f0c943fc69b4a8ef248c
MITHUN M R <mithunmr286@gmail.com>
2024-02-14 11:05:18 +0530

    Initial Commit

---
+++ MedBook/MedBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedBookApp.swift
+//  MedBook
+//
+//  Created by Mithun M R on 14/02/24.
+//
+
+import SwiftUI
+
+@main
+struct MedBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
